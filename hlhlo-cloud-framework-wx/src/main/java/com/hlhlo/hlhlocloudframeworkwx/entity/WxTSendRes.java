3d93c46dfe398e5312fa4750e0f993b60705fc1d
付 <543163041@qq.com>
2018-10-30 10:41:15 +0800

    sss

--- hlhlo-cloud-framework-wx/src/main/java/com/hlhlo/hlhlocloudframeworkwx/entity/WxTSendRes.java
+++ hlhlo-cloud-framework-wx/src/main/java/com/hlhlo/hlhlocloudframeworkwx/entity/WxTSendRes.java
@@ -2,11 +2,13 @@ package com.hlhlo.hlhlocloudframeworkwx.entity;
 
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 发送模板信息接收到的信息
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class WxTSendRes extends BaseResponse {
     private String msgid;
 }
